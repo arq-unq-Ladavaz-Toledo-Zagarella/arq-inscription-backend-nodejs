6951c901db59b9c4fd9f3f65f2529695f9ecc98e
luis-zagarella <zagarojo23@gmail.com>
2017-11-12 23:05:41 -0300

    cambio del puerto de servidor

--- src/backend/server.js
+++ src/backend/server.js
@@ -5,5 +5,7 @@ mongoose.connect('mongodb://root:root@ds249605.mlab.com:49605/arquitectura')
 //mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/careers')
 
 // Express startup
-const port = 3001
+const port = process.env.PORT
+//const port = 3001
+
 app.listen(port, () => console.log(`Server running on port ${port}`))
