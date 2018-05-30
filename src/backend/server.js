2fe06c2a08b471c317254a30aad8f9d025ea7d35
sladavaz <santiagoladavaz@gmail.com>
2018-05-30 12:15:22 -0300

    Se agrega Loggly

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,11 +1,37 @@
 import app from "./app"
+import swaggerTools from 'swagger-tools';
+import swaggerDoc from './swagger.json';
+import routes from './routes/routes.js';
 
 import mongoose from 'mongoose'
-//mongoose.connect('mongodb://root:root@ds249605.mlab.com:49605/arquitectura')
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/careers')
+mongoose.connect('mongodb://root:root@ds249605.mlab.com:49605/arquitectura')
+//mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/careers')
 
 // Express startup
 //const port = process.env.PORT
 const port = 3001
 
-app.listen(process.env.PORT || port)
+
+app.disable('etag');
+
+const optionsSwagger = { swaggerUi: '/swagger.json', controllers: './src/backend/routes' };
+
+swaggerTools.initializeMiddleware(swaggerDoc, middleware => {
+    app.use(middleware.swaggerMetadata());
+    app.use(middleware.swaggerValidator());
+    app.use(middleware.swaggerRouter(optionsSwagger));
+    app.use(middleware.swaggerUi());
+	app.listen(process.env.PORT || port);
+});
+
+var winston = require('winston');
+require('winston-loggly-bulk');
+ 
+ winston.add(winston.transports.Loggly, {
+    token: "a17443aa-eeb0-40ad-a7e8-8dd5ef2e903f",
+    subdomain: "sladavaz",
+    tags: ["Winston-NodeJS"],
+    json:true
+});
+
+winston.log('info',"Start app");
