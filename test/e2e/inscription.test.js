22c58bd7c26e88d9de8a8eebed1e304b09dfaeb6
Luis Zagarella <zagarojo23@gmail.com>
2018-07-02 15:36:08 -0300

    fix en la url del test

--- test/e2e/inscription.test.js
+++ test/e2e/inscription.test.js
@@ -9,8 +9,9 @@ describe("Create an inscription", () => {
 
   it("Should add a inscription from a student", async() => {
 
-   browser.get("http://inscripciones-unq.herokuapp.com/#/question/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjVhN2M3MDRkNGFhMzYxMjMyY2JiMGQyNSJ9.Hurt71UJSHE-hLeM04WCXcK0-a3V4463eol5gwjskYw") 
+   browser.get("http://inscripciones-unq.herokuapp.com/#/question/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjVhMjRhYTIyMjdjZjkzOTI0YmQwYWZhYyJ9.lkHHEQ-WQFqiICXttpm6dihoiac9J2iIgeEldUm9jBU")
 
+   //eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjVhN2M3MDRkNGFhMzYxMjMyY2JiMGQyNSJ9.Hurt71UJSHE-hLeM04WCXcK0-a3V4463eol5gwjskYw"
     await element.all(by.css(".chooseSubject")).last().click()
 
     await element.all(by.css(".chooseCourse")).last().click()
